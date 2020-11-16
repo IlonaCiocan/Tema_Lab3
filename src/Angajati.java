f08642db7b23b1d00e8ccd5e0ea156169b51a044
ilona <ilona.clocan@mta.ro>
2020-11-16 15:09:29 +0200

    Adaugare clasa Angajati
    Am adaugat clasa Angajati impreuna cu constructorul
    
    Signed-off-by: ilona <ilona.clocan@mta.ro>

---
+++ src/Angajati.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Angajati {
+
+    String nume;
+    String prenume;
+    String nr_tel;
+    int durata_contract;
+    int salariu;
+
+
+    public Angajati(String nume, String prenume, String nr_tel, int durata_contract, int salariu) {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.nr_tel = nr_tel;
+        this.durata_contract = durata_contract;
+        this.salariu = salariu;
+    }
+
+    
+
+
+}
